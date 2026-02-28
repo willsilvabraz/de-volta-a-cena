916b155aee6eae446736a976f3f6270e7e9ba16f
willsilvabraz <willsilvabraz@gmail.com>
2026-02-27 22:50:28 -0400

    Criação de controler

---
+++ src/controller/Pessoas.js
@@ -0,0 +1,23 @@
+const ModeloPessoa = require('../models/Pessoas');
+
+class ControllerPessoa {
+  static async Cadastro(req, res) {
+    try {
+      const id = await ModeloPessoa.Cadastro({
+        nome: "Willia",
+        profissao: "Dev",
+        criadoEm: new Date()
+      });
+
+      res.status(201).json({
+        message: "Cadastrado com sucesso",
+        id: id
+      });
+
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
+
+module.exports = ControllerPessoa;
